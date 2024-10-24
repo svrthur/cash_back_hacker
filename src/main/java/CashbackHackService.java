cf5dfe4f528b2e0a56012516a10168d549d51e40
arthur.salahutdinov <artes_93@mail.ru>
2024-10-24 23:43:34 +0300

    Initial commit with CashbackHackService and tests

--- src/main/java/CashbackHackService.java
+++ src/main/java/CashbackHackService.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+package ru.netology.service;
 
 public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        if (amount % boundary == 0) {
+            return boundary; // намеренная ошибка
+        }
+        return boundary - amount % boundary;
+    }
 }
--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturn100IfAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldFailTestIfAmountIs900() {  // Специальный падающий тест
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 200;  // Неправильное ожидаемое значение
+
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected);  // Этот тест намеренно упадет
+    }
 }
